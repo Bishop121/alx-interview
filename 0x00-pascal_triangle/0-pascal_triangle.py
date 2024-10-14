8ce06b5808e54de17335fa988aa9feb59c13d35d
Bishop121 <obidikeemmanuel@outlook.com>
2024-10-14 12:07:03 +0100

    Pascal_trangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,46 +1,21 @@
 #!/usr/bin/python3
-    """function that returns a list of lists of integers
-    representing the Pascal’s triangle of n
-    """
-
-
+"""this is a pascal triangle generator file"""
 def pascal_triangle(n):
-  """
-   Args
-  n(int): length of triangle (assume n will be always an integer)
-  return: list of lists representing the Pascal’s triangle of n  """
- if n <= 0:
- return []
-
- if n == 1:
- ret_list = pascal_triangle(0)
- ret_list.append([1])
- return ret_list
-
- if n == 2:
- ret_list = pascal_triangle(1)
- ret_list.append([1, 1])
- return ret_list
-
- if n > 2:
-ret_list = pascal_triangle(2)
-for i in range(n-2):    
-new_row = []
-last_row = ret_list[-1]
-for index, value in enumerate(last_row):
-if index == 0:
-new_row.append(value)
-next_value = last_row[index + 1]
-new_row.append(value + next_value)
-
-                                                                                                                                                                                                                    elif index == len(last_row)-1:                                                                                                                                                                                     new_row.append(value)
-
-                                                                                                                                                                                                                    else:
-                                                                                                                                                                                                                    next_value = last_row[index + 1]
-                                                                                                                                                                                                                    new_row.append(value + next_value)
-
-                                                                                                                                                                                                                    ret_list.append(new_row)
-
-                                                                                                                                                                                                                
-return ret_list
-
+  """ a function that prints a pascal triangle of n number of rows
+ a function def pascal_triangle(n): that returns a list of lists
+ of integers representing the Pascal’s triangle of n:
+   """
+ res = []
+if n > 0:
+ """executes only if n is greater than 0
+ """
+for i in range(1, n + 
+"loop should run n amount of time
+"""
+new_list = []
+C = 1
+for j in range(1, i + 1):
+new_list.append(C)
+C = C * (i - j) // j
+res.append(new_list)
+return res
